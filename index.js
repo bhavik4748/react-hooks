37249da1087e5f97d3e693eb2b78f24b7851829d
bhavik4748 <bhavik4748@gmail.com>
2020-06-24 22:22:11 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Hello } from "./Hello";
+import "./style.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+  state = {
+    name: "Mr. React"
+  };
 
   render() {
     return (
       <div>
         <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <p>Start editing to see some magic happen :)</p>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
